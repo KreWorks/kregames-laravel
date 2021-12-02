a1f78a0639e0ea6b2a24f0a8c1a01d055a902f52
Császár-Kozma Réka <rekuci86@gmail.com>
2021-12-02 13:44:55 +0100

    Add ArtisanController

---
+++ app/Http/Controllers/Admin/ArtisanController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
+
+class ArtisanController extends BaseController
+{
+    public function index()
+    {
+
+    }
+
+    public function routerefresh()
+    {
+
+    }
+
+    public function viewrefrest()
+    {
+
+    }
+
+    public function configrefresh()
+    {
+
+    }
+
+    public function migrate()
+    {
+        
+    }
+}
