6ff38410ed214762eeefe63b908de0ac0b5ef77f
Császár-Kozma Réka <rekuci86@gmail.com>
2022-01-13 12:30:58 +0100

    Refactor modal and form blade structure

--- resources/views/admin/_layout/list.blade.php
+++ resources/views/admin/_layout/list.blade.php
@@ -17,5 +17,18 @@
             </div>
         </div>
     </div>
-    @include('admin.'.$route.'.modal')
+    <!-- Add Jam Modal-->
+    <div class="modal fade" id="{{$name}}Form" tabindex="-1" aria-labelledby="{{$name}}FormLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header bg-secondary">
+                    <h5 class="modal-title" id="{{$name}}FormTitlelabel">Játék létrehozása</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                @include('admin.'.$route.'.form')
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/admin/games/form.blade.php
+++ resources/views/admin/games/form.blade.php
@@ -1,69 +1,43 @@
-@extends('admin._layout.login_layout')
+<form method="POST" action="{{ route('admin.games.create') }}" enctype="multipart/form-data" name="game-form" id="game-form">
+    @csrf
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group col-lg-12">
+                <label for="name" class="col-form-label">Név</label>
+                <input class="form-control" type="text" value="" id="name" name="name" onkeyup="document.getElementById('slug').value = createSlug(value)">
+            </div>
+            <div class="form-group col-lg-12">
+                <label for="slug" class="col-form-label">Slug</label>
+                <input class="form-control" type="text" value="" id="slug" name="slug">
+            </div>
+            <div class="form-group col-lg-12">
+                <label for="publish_date" class="col-form-label">Megjelenés dátuma</label>
+                <input class="form-control" type="datetime-local" value="{{ time() }}" id="publish_date" name="publish_date">
+            </div>
+            <div class="form-group col-lg-12">
+                <label for="jam_id" class="col-form-label">Jam</label>
+                <select class="form-control" id="jam_id" name="jam_id">
+                    <option value="0" selected>Nincs jam</option>
+                    @foreach($extraDatas['jams'] as $jam)
+                        <option value="{{$jam->id}}">{{$jam->name}}</option>
 
-@section('content')
-    <div class="main-content-inner">
-        <div class="row">
-            <div class="col-lg-12 col-ml-12 mt-5">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="header-title">Játék létrehozása</h4>
-                    </div>
-                    <div class="card-body d-flex">
-                        <div class="col-6">
-                        @if ($entity)
-                        <form method="POST" action="{{ route($formAction, $entity->id) }}" enctype="multipart/form-data">
-                            @method('PUT')
-                        @else 
-                        <form method="POST" action="{{ route($formAction) }}" enctype="multipart/form-data">
-                        @endif
-                            @csrf
-                            <div class="form-group col-lg-12">
-                                <label for="name" class="col-form-label">Név</label>
-                                <input class="form-control" type="text" value="{{ $entity ? $entity->name : '' }}" id="name" name="name" onkeyup="document.getElementById('slug').value = createSlug(value)">
-                            </div>
-                            <div class="form-group col-lg-12">
-                                <label for="slug" class="col-form-label">Slug</label>
-                                <input class="form-control" type="text" value="{{ $entity ? $entity->slug : '' }}" id="slug" name="slug">
-                            </div>
-                            <div class="form-group col-lg-12">
-                                <label for="publish_date" class="col-form-label">Megjelenés dátuma</label>
-                                <input class="form-control" type="datetime-local" value="{{ $entity ? preg_replace('/ /', 'T', $entity->publish_date) : time() }}" id="publish_date" name="publish_date">
-                            </div>
-                            <div class="form-group col-lg-12">
-                                <label for="jam_id" class="col-form-label">Jam</label>
-                                <select class="form-control" id="jam_id" name="jam_id">
-                                    <option value="0" {{ $entity ? '' : 'selected'}}>Nincs jam</option>
-                                    @foreach($jams as $jam)
-                                        @if($entity && $jam == $entity->jam)
-                                        <option value="{{$jam->id}}" selected>{{$jam->name}}</option>
-                                        @else
-                                        <option value="{{$jam->id}}">{{$jam->name}}</option>
-                                        @endif
-                                    @endforeach
-                                <select>
-                            </div>
-                            <div class="col-auto my-1">
-                                <button type="submit" class="btn btn-primary col-6">Mentés</button>
-                            </div>
-
-                        </div>
-                        <div class="col-6">
-                            <div class="form-group col-lg-12">
-                                <label for="icon" class="form-label">Logó</label>
-                                <input class="form-control" type="file" id="icon" name="icon">
-                            </div>
-                            @if ($entity)
-                            <div class="col-lg-12">
-                                <img class="col-lg-3" src="/{{ $entity->__get('iconPath') }}">
-                            </div>
-                            @endif
-                        </div>
-                        </form>
-                    </div>
-                </div>
+                    @endforeach
+                    <select>
             </div>
         </div>
+        <div class="col-md-6">
+            <div class="form-group col-lg-12">
+                <label for="icon" class="form-label">Logó</label>
+                <input class="form-control" type="file" id="icon" name="icon">
+            </div>
+            <div class="col-lg-12">
+                <img class="col-lg-3" src="/">
+            </div>
+
+        </div>
     </div>
-</div>
-<!-- main content area end -->
-@endsection
+    <div class="modal-footer mt-3">
+        <button type="submit" class="btn btn-primary" >Mentés</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Bezár</button>
+    </div>
+</form>
