c2a810a51e41e37f9397176606eea87fce79615c
Császár-Kozma Réka <rekuci86@gmail.com>
2022-02-14 12:29:02 +0100

    Add links to game edit page and linktypes listpage

--- resources/views/admin/games/edit.blade.php
+++ resources/views/admin/games/edit.blade.php
@@ -15,6 +15,26 @@
                 @include('admin.games.form')
             </div>
         </div>
+        <hr />
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <div class="row">
+                    <span class="col-md-8 my-auto">Link lista</span>
+                    <button type="button" class="btn btn-warning col-md-3 float-right" id="addLinkButton"
+                            onclick="openModal('links', 'link', 'create', '{{route('admin.links.store')}}')">
+                        Új link hozzáadása
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <?php 
+                $tableLabels = App\Models\Link::$tableLabels;
+                $datas = $entity->links;
+                ?>
+                @include('admin._parts.table_list')
+
+            </div>
+        </div>
     </div>
 
 @endsection
