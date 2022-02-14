5a62c9e90b470e21e14d544d0825bf05eb6920d3
Császár-Kozma Réka <rekuci86@gmail.com>
2022-02-14 12:37:39 +0100

    Add linktypes add new modal

--- resources/views/admin/linktypes/form.blade.php
+++ resources/views/admin/linktypes/form.blade.php
@@ -1,28 +1,37 @@
-<form method="POST" action="{{ route('admin.linktypes.create') }}" enctype="multipart/form-data" name="linktype-form" id="linktype-form">
+@if (isset($entity))
+<form method="POST" action="{{ route('admin.linktypes.update', $entity->id) }}" enctype="multipart/form-data" name="linktype-form" id="linktype-form">
+    @method('PUT')
+@else 
+<form method="POST" action="{{ route('admin.linktypes.store')}}" enctype="multipart/form-data" name="linktype-form" id="linktype-form">
+@endif
     @csrf <!-- {{ csrf_field() }} -->
     <div class="row">
         <div class="col-md-12">
             <div class="form-group col-lg-12">
                 <label for="name" class="col-form-label">Név</label>
-                <input class="form-control" type="text" value="" id="name" name="name" >
+                <input class="form-control" type="text" value="{{ isset($entity) ? $entity->name : ''}}" id="name" name="name" >
             </div>
             <div class="form-group col-lg-12">
                 <label for="hover_text" class="col-form-label">Hover szöveg</label>
-                <input class="form-control" type="text" value="" id="hover_text" name="hover_text">
+                <input class="form-control" type="text" value="{{ isset($entity) ? $entity->hover_text : ''}}" id="hover_text" name="hover_text">
             </div>
             <div class="form-group col-lg-12">
                 <label for="fontawesome" class="col-form-label">Font Awesome</label>
-                <input class="form-control" type="text" value="" id="fontawesome" name="fontawesome">
+                <input class="form-control" type="text" value="{{ isset($entity) ? $entity->fontawesome : ''}}" id="fontawesome" name="fontawesome">
             </div>
             <div class="form-group col-lg-12">
                 <label for="color" class="col-form-label">Szín</label>
-                <input class="form-control" type="color" value="" id="color" name="color" style="height:50px;width:100px">
-            </div>
-
-            <div class="modal-footer mt-3">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Bezár</button>
-                <button type="submit" class="btn btn-primary" data-bs-dismiss>Mentés</button>
+                <input class="form-control" type="color" value="{{ isset($entity) ? $entity->color : ''}}" id="color" name="color" style="height:50px;width:100px">
             </div>
         </div>                 
+    </div>
+    @if (isset($entity))
+    <div class="modal-footer mt-3">
+        <a href="{{route('admin.linktypes.index')}}"  class="btn btn-secondary">Vissza</a>
+    @else
+    <div class="modal-footer mt-3">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Bezár</button>
+    @endif
+        <button type="submit" class="btn btn-primary" >Mentés</button>
     </div>
 </form>
