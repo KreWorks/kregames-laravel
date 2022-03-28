68adaed2bdacdd5d3e9e27246e50c98693741d12
Császár-Kozma Réka <rekuci86@gmail.com>
2022-03-28 11:42:39 +0200

    Add filter by user to game search

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Game;
+use App\Models\User;    
 
 class IndexController extends Controller
 {
     public function index()
     {
-        $games = Game::all();
+        $user = User::find(1);
+        $games = Game::where("user_id", 1)->get(); 
 
         return view('main.index', ['games' => $games]);
     }
